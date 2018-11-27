ddd70e3919c0aebd12d6d67a3fc5d814b21983c9
dee <dee@adadevelopersacademy.org>
2018-11-27 11:07:27 -0800

    students livecode: components, props, css, and proptypes

---
+++ src/components/Student.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Student.css';
+
+const Student = (props) => {
+
+  const name = props.fullName;
+
+  return (
+    <section className="student">
+      <h3>Student</h3>
+      <ul>
+        <li>Name: {name}</li>
+        <li>Email: {props.email}</li>
+        <li>Hometown: {props.hometown}</li>
+        <li>Class: {props.classroom}</li>
+      </ul>
+    </section>
+  )
+};
+
+
+Student.propTypes = {
+  fullName: PropTypes.string.isRequired,
+  email: PropTypes.string,
+};
+
+
+export default Student;
---
+++ src/components/StudentCollection.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './StudentCollection.css';
+import Student from './Student';
+
+const StudentCollection = () => {
+
+  const students = [
+    {
+      fullName: "Ada Lovelace",
+      email: "ada@lovelace.uk",
+    },
+    {
+      fullName: "Katherine Johnson",
+      email: "kat@nasa.gov",
+    }
+  ];
+
+  const studentCollection = students.map((student, i) => {
+    return <Student key={i} fullName={student.fullName} email={student.email} />
+  });
+
+  const header = "header";
+
+  const headerColor = students.length > 1 ? "red-header" : "blue-header";
+
+  return (
+    <section>
+      <h2 className={header + " " + headerColor}>Students</h2>
+      <ul>
+        {studentCollection}
+      </ul>
+    </section>
+  );
+};
+
+export default StudentCollection;
