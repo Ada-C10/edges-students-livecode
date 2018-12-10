0457d9a6956126604fbc9403a9239d4545be56e6
Dan Roberts <dan@adadevelopersacademy.org>
2018-12-10 14:06:41 -0800

    Convert uncontrolled form to controlled form

--- src/components/NewStudentForm.js
+++ src/components/NewStudentForm.js
@@ -4,18 +4,41 @@ import  './NewStudentForm.css';
 class NewStudentForm extends Component {
   constructor() {
     super();
+
+    this.state = {
+      fullName: "",
+      email: ""
+    }
   }
+
+  onNameChange = (event) => {
+    console.log("Name was changed", event);
+    this.setState({fullName: event.target.value})
+  }
+
+  onEmailChange = (event) => {
+    this.setState({email: event.target.value})
+  }
+
   render() {
     return (
       <div>
         <form className="new-student-form">
           <div>
             <label htmlFor="fullName">Name:</label>
-            <input name="fullName" />
+            <input
+              name="fullName"
+              value={this.state.fullName}
+              onChange={this.onNameChange}
+              />
           </div>
           <div>
             <label htmlFor="email">Email:</label>
-            <input name="email" />
+            <input
+              name="email"
+              value={this.state.email}
+              onChange={this.onEmailChange}
+              />
           </div>
           <input
             type="submit"
